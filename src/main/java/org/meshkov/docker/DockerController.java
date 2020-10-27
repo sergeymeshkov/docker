4d25b0de302f618c178b384c2bc98176d7110d24
Sergey Meshkov <siarhei.miashkou@leverx.com>
2020-10-27 23:07:09 +0300

    Initial commit

---
+++ src/main/java/org/meshkov/docker/DockerController.java
@@ -0,0 +1,16 @@
+package org.meshkov.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class DockerController {
+
+	@GetMapping
+	public String sayHiToDocker() {
+		return "Hi, Docker!!!";
+	}
+
+}
